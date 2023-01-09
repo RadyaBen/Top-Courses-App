1cbfa722bfbe883b03cedd71f12962f662aeda01
RadyaBen <lizoguben.radya@gmail.com>
2023-01-09 17:40:48 +0200

    Add styles to the footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,15 +1,21 @@
+import cn from 'classnames';
 import { format } from 'date-fns';
 
 import { FooterProps } from './Footer.props';
 
-export const Footer = ({ ...props }: FooterProps) => {
+import styles from './Footer.module.css';
+
+export const Footer = ({ className, ...props }: FooterProps) => {
 	return (
-		<footer {...props}>
+		<footer
+			className={cn(className, styles.footer)}
+			{...props}
+		>
 			<div>
 				Copyright © 2022 - {format(new Date(), 'yyyy')} OwlTop
 			</div>
-			<a href="" target='_blank'>Terms of Use</a>
-			<a href="" target='_blank'>Privacy Policy</a>
+			<a href='#' target='_blank'>Terms of Use</a>
+			<a href='#' target='_blank'>Privacy Policy</a>
 		</footer>
 	);
 };
