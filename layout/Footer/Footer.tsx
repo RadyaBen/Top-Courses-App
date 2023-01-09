f1e50a9f2a4ee28e7d8b4276a6282d6c8a6e76dd
RadyaBen <lizoguben.radya@gmail.com>
2023-01-09 15:10:10 +0200

    Add elements to the footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,15 @@
+import { format } from 'date-fns';
+
 import { FooterProps } from './Footer.props';
 
 export const Footer = ({ ...props }: FooterProps) => {
 	return (
-		<>
-			<div {...props}>
-				Footer
+		<footer {...props}>
+			<div>
+				Copyright © 2022 - {format(new Date(), 'yyyy')} OwlTop
 			</div>
-		</>
+			<a href="" target='_blank'>Terms of Use</a>
+			<a href="" target='_blank'>Privacy Policy</a>
+		</footer>
 	);
 };
